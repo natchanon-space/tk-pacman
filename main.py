91405d74ce703be0b7718dd29161928adfaafc5d
Natchanon Manatphaiboon <natchanon.space@gmail.com>
2021-03-18 10:29:51 +0700

    scoring is working but I don't know it is observer pattern or not

--- main.py
+++ main.py
@@ -21,6 +21,8 @@ class Pacman(Sprite):
         self.direction = DIR_STILL
         self.next_direction = DIR_STILL
 
+        self.dot_eaten_observers = []
+
         x, y = maze.piece_center(r,c)
         super().__init__(app, 'images/pacman.png', x, y)
 
@@ -30,6 +32,9 @@ class Pacman(Sprite):
 
             if self.maze.has_dot_at(r, c):
                 self.maze.eat_dot_at(r, c)
+                # TODO: 
+                #   - call all the observers here
+                self.dot_eaten_observers[0]()
             
             if self.maze.is_movable_direction(r, c, self.next_direction):
                 self.direction = self.next_direction
@@ -58,6 +63,12 @@ class PacmanGame(GameApp):
 
         self.pacman1_score = 0
         self.pacman2_score = 0
+
+        # TODO:
+        #   - register self.dot_eaten_by_pacman1 to self.pacman1's observers
+        #   - register self.dot_eaten_by_pacman2 to self.pacman2's observers
+        self.pacman1.dot_eaten_observers.append(self.dot_eaten_by_pacman1)
+        self.pacman2.dot_eaten_observers.append(self.dot_eaten_by_pacman2)
 
     def update_scores(self):
         self.pacman1_score_text.set_text(f'P1: {self.pacman1_score}')
